f57d7db2ede1c4c9a4975ea59cea28ebaf27f0f0
Akanksha Indap <77394820+akankshaindap@users.noreply.github.com>
2024-01-01 21:38:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let count=0;
+let saveEl=document.getElementById("save-el");
+let countEl=document.getElementById("count-el");
+function increment(){
+    count+=1;
+    countEl.textContent= count;
+}
+function save() { 
+    let countStr= count+ " - "
+    saveEl.textContent+=countStr
+    countEl.textContent=0
+    count=0
+}
+console.log("People entered are");
